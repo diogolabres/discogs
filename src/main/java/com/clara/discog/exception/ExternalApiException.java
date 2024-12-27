d5c15ae8f98a16010cce034d7332918f0ac44d04
Diogo Labres <diogo.labres@clara.team>
2024-12-27 16:48:40 -0300

    add

--- src/main/java/com/clara/discog/exception/ExternalApiException.java
+++ src/main/java/com/clara/discog/exception/ExternalApiException.java
@@ -1,5 +1,9 @@
 package com.clara.discog.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
 public class ExternalApiException extends RuntimeException {
     public ExternalApiException(String message, Throwable cause) {
         super(message, cause);
